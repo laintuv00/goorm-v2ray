641e7863244e96811523b81622731ca4c8b0ccb0
laintuv00 <laintuv00@gmail.com>
2020-08-17 08:46:12 +0700

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#下载核心程序
+#Download core program
 mkdir /v2ray
 cd /v2ray
 rm -f /v2ray/v2ray
 wget https://github.com/laintuv00/goorm-v2ray/raw/master/v2ray
-#设置运行权限
+#Set running permissions
 chmod +x /v2ray/v2ray
 rm -f /v2ray/v2ctl
 wget https://github.com/laintuv00/goorm-v2ray/raw/master/v2ctl
 chmod +x /v2ray/v2ctl
 rm -f /v2ray/config.json
 wget https://github.com/laintuv00/goorm-v2ray/raw/master/config.json
-# 开始后台运行
+# Start running in the background
 nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
